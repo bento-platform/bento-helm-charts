0cb8efc5235253e43911d7cca93c31ed5f77b28c
Victor Rocheleau <victor.rocheleau@mcgill.ca>
2026-07-02 14:36:26 -0400

    use cli cloud-provider-kind for macos networking

--- scripts/kind-cluster-init.bash
+++ scripts/kind-cluster-init.bash
@@ -1,4 +1,11 @@
 kind create cluster
 
-VERSION=v0.11.0
-docker run -d --name cloud-provider-kind --rm --network host -v /var/run/docker.sock:/var/run/docker.sock registry.k8s.io/cloud-provider-kind/cloud-controller-manager:${VERSION}
+# TODO: Running Cloud-Provider-Kind in Docker works well on Linux but is having issues on MacOS.
+#       Testing with the CLI tool instead so it runs as sudo, works well on Linux.
+# 
+# VERSION=v0.11.0
+# sudo docker run -d --name cloud-provider-kind --rm --network host \
+#     -v /var/run/docker.sock:/var/run/docker.sock registry.k8s.io/cloud-provider-kind/cloud-controller-manager:${VERSION}
+
+# Sudo required for MacOS host port mapping
+sudo cloud-provider-kind
