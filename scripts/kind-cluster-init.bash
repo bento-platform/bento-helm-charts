8aeda8fdb4c8c93849ba38b1ae98b60ae1c603a2
v-rocheleau <victor.rocheleau@mcgill.ca>
2026-07-07 10:18:56 -0400

    chore(scripts): kind cloud provider multi os support

--- scripts/kind-cluster-init.bash
+++ scripts/kind-cluster-init.bash
@@ -1,12 +1,20 @@
 kind create cluster
 
 # Running Cloud-Provider-Kind in Docker works well on Linux but is having issues on MacOS.
-#       Testing with the CLI tool instead so it runs as sudo, works well on Linux.
-
-# Cloud-Provider-Kind can be ran on Docker on Linux without issue:
-# VERSION=v0.11.0
-# sudo docker run -d --name cloud-provider-kind --rm --network host \
-#     -v /var/run/docker.sock:/var/run/docker.sock registry.k8s.io/cloud-provider-kind/cloud-controller-manager:${VERSION}
-
-# Sudo required for MacOS host port mapping
-sudo cloud-provider-kind
+case "$(uname -s)" in
+    Darwin)
+        # MacOS Cloud-Provider-Kind has to be ran with the CLI using sudo
+        # Keep this terminal running!
+        sudo cloud-provider-kind
+        ;;
+    Linux)
+        # Linux supports running Cloud-Provider-Kind in a container
+        VERSION=v0.11.0
+        sudo docker run -d --name cloud-provider-kind --rm --network host \
+            -v /var/run/docker.sock:/var/run/docker.sock registry.k8s.io/cloud-provider-kind/cloud-controller-manager:${VERSION}
+        ;;
+    *)
+        echo "Unsupported OS"
+        exit 1
+        ;;
+esac
